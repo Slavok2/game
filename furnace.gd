38816abd1f02e219ec32c063acd85630a83225d2
SlavokCheese <srugge2409@gmail.com>
2025-07-14 15:25:03 +0300

    .

--- furnace.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-@onready var mesh_instance_3d: MeshInstance3D = $"../RigidBody3D/MeshInstance3D"
-
-func _process(delta: float) -> void:
-	pass
--- player/fp_cam.gd
+++ player/fp_cam.gd
@@ -1,9 +1,8 @@
 extends Node3D
 
 var sensitivity = 0.2
-@onready var raycast = $Camera3D/RayCast3D
+@onready var raycast: RayCast3D = $Camera3D/RayCast3D
 @onready var hand: Node3D = $Hand
-
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -17,11 +16,11 @@ func _input(event: InputEvent) -> void:
 		get_tree().quit()
 
 func _process(delta: float) -> void:
-	var object = raycast.get_collider()
 	if raycast.is_colliding():
-		if object.is_in_group("pickable"):
+		var collider = raycast.get_collider()
+
+		if collider and collider.is_in_group("pickable"):
 			if Input.is_action_pressed("interact"):
-				object.global_transform = hand.global_transform
-				object.global_rotation = hand.global_rotation
-				object.collision_layer = 2
-				object.linear_velocity = Vector3(0.1, 3, 0)
+				if collider is RigidBody3D:
+					collider.global_transform = hand.global_transform
+					collider.linear_velocity = Vector3(0.1, 3, 0)
